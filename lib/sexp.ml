4804b1970feb2512bcc3db05cfcc9eaa4c5f740f
Guillaume Petiot <guillaume@tarides.com>
2021-09-26 00:29:28 +0800

    Setup ocamlformat

--- lib/sexp.ml
+++ lib/sexp.ml
@@ -8,15 +8,15 @@ let pp_atom fs = function
 
 let rec pp fs = function
   | List x ->
-    Format.pp_print_list pp fs x
-      ~pp_sep:(fun fs () -> Format.fprintf fs "@ ")
-  | Atom x ->
-    pp_atom fs x
+      Format.pp_print_list pp fs x ~pp_sep:(fun fs () -> Format.fprintf fs "@ ")
+  | Atom x -> pp_atom fs x
 
 let of_lexbuf lx =
   match Parser.sexp_list Lexer.token lx with
-  | exception (Lexer.Error (err, loc)) ->
-    Error (`Msg (Format.asprintf "%a%a" Location.print_loc loc Lexer.pp_error err))
+  | exception Lexer.Error (err, loc) ->
+      Error
+        (`Msg
+          (Format.asprintf "%a%a" Location.print_loc loc Lexer.pp_error err))
   | x -> Ok x
 
 let of_string s = of_lexbuf (Lexing.from_string s)
--- lib/sexp.mli
+++ lib/sexp.mli
@@ -2,6 +2,6 @@ include module type of Sexp_intf
 
 val pp : Format.formatter -> t -> unit
 
-val of_lexbuf : Lexing.lexbuf -> (t list, [`Msg of string]) result
+val of_lexbuf : Lexing.lexbuf -> (t list, [ `Msg of string ]) result
 
-val of_string : string -> (t list, [`Msg of string]) result
+val of_string : string -> (t list, [ `Msg of string ]) result
