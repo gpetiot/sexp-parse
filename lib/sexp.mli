6db5b02353e9fabf22c006d7b43ccdf6a7b3e87c
Guillaume Petiot <guillaume@tarides.com>
2021-10-02 12:38:34 +0100

    Expose equal function for typed s-expressions (#5)

--- lib/sexp.mli
+++ lib/sexp.mli
@@ -2,6 +2,8 @@ type atom = Symbol of string | String of string | Int of int | Float of float
 
 type t = List of t list | Atom of atom
 
+val equal : t -> t -> bool
+
 val dump : Format.formatter -> t -> unit
 
 val pp : Format.formatter -> t -> unit
