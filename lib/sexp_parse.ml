bd48e463e97698158437b7a08f438f909822bd7e
Guillaume Petiot <guillaume@tarides.com>
2021-10-02 18:57:58 +0800

    Factorize typed/untyped of_lexbuf functions

--- lib/sexp_parse.ml
+++ lib/sexp_parse.ml
@@ -1,39 +1,41 @@
+module Make (Sexp : sig
+  type t
+end) (Parser : sig
+  type token
+
+  exception Error
+
+  val sexp_list : (Lexing.lexbuf -> token) -> Lexing.lexbuf -> Sexp.t list
+end) (Lexer : sig
+  type error
+
+  exception Error of error * Location.t
+
+  val pp_error : Format.formatter -> error -> unit
+
+  val token : Lexing.lexbuf -> Parser.token
+end) =
+struct
+  let of_lexbuf lx =
+    match Parser.sexp_list Lexer.token lx with
+    | exception Lexer.Error (err, loc) ->
+        Error
+          (`Msg
+            (Format.asprintf "%a: %a" Location.print_loc loc Lexer.pp_error err))
+    | exception Parser.Error ->
+        let pos = lx.lex_curr_p in
+        Error
+          (`Msg
+            (Format.asprintf "Line:%d Column:%d: syntax error@." pos.pos_lnum
+               (pos.pos_cnum - pos.pos_bol)))
+    | x -> Ok x
+
+  let of_string s = of_lexbuf (Lexing.from_string s)
+end
+
 module Typed = struct
   include Sexp
-
-  let of_lexbuf lx =
-    match Sexp_parser.sexp_list Sexp_lexer.token lx with
-    | exception Sexp_lexer.Error (err, loc) ->
-        Error
-          (`Msg
-            (Format.asprintf "%a: %a" Location.print_loc loc Sexp_lexer.pp_error
-               err))
-    | exception Sexp_parser.Error ->
-        let pos = lx.lex_curr_p in
-        Error
-          (`Msg
-            (Format.asprintf "Line:%d Column:%d: syntax error@." pos.pos_lnum
-               (pos.pos_cnum - pos.pos_bol)))
-    | x -> Ok x
-
-  let of_string s = of_lexbuf (Lexing.from_string s)
+  include Make (Sexp) (Sexp_parser) (Sexp_lexer)
 end
 
-module Untyped = struct
-  let of_lexbuf lx =
-    match Sexp0_parser.sexp_list Sexp0_lexer.token lx with
-    | exception Sexp0_lexer.Error (err, loc) ->
-        Error
-          (`Msg
-            (Format.asprintf "%a: %a" Location.print_loc loc
-               Sexp0_lexer.pp_error err))
-    | exception Sexp0_parser.Error ->
-        let pos = lx.lex_curr_p in
-        Error
-          (`Msg
-            (Format.asprintf "Line:%d Column:%d: syntax error@." pos.pos_lnum
-               (pos.pos_cnum - pos.pos_bol)))
-    | x -> Ok x
-
-  let of_string s = of_lexbuf (Lexing.from_string s)
-end
+module Untyped = Make (Sexplib0.Sexp) (Sexp0_parser) (Sexp0_lexer)
